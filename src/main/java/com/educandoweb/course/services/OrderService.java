7891351a290c29c1a94c6168c95c32b2be514cd7
Jonilson da Silva Leles <jsleles@gmail.com>
2025-05-18 10:11:47 -0300

    OrderStatus enum

--- src/main/java/com/educandoweb/course/services/OrderService.java
+++ src/main/java/com/educandoweb/course/services/OrderService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.educandoweb.course.entities.Order;
+import com.educandoweb.course.entities.User;
 import com.educandoweb.course.repositories.OrderRepository;
 
 @Service
